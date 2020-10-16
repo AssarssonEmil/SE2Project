f679c521d9f7b4513ef5a54c836ccf8bad25080f
JimboMan123 <tauriainenjimmie@live.se>
2020-10-16 14:35:11 +0200

    added Views

---
+++ app/src/main/java/com/hkr/Views/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.hkr.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hkr.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
